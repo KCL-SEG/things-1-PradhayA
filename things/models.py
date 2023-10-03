5db7645d9a04785d2667ebbae3bcb489ab7cd918
PradhayA <meetpradhay@gmail.com>
2023-10-04 00:32:07 +0100

    remove commas

--- things/models.py
+++ things/models.py
@@ -1,12 +1,13 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
-
 class Thing(models.Model):
-    name = models.CharField(max_length=30, unique=True, blank=False),
-    description = models.TextField(blank=True, max_length=120),
-    quantity = models.IntegerField(        validators=[
+    name = models.CharField(max_length=30, unique=True, blank=False)
+    description = models.TextField(blank=True, max_length=120)
+    quantity = models.IntegerField(
+        validators=[
             MaxValueValidator(100),
             MinValueValidator(0)
-        ])
+        ]
+    )
+
